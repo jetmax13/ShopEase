e0a71caf6fc302ace15d37d2e6b03bc97206982f
jetmax13 <vaibhavduggal03@gmail.com>
2024-04-21 05:13:30 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoute.js'
+dotenv.config();
+
+connectDB();
+
+const app=express()
+
+//middlewares
+app.use(express.json())
+app.use(morgan('dev'))
+
+//routes
+app.use('/api/v1/auth',authRoutes)
+
+app.get('/',(req,res)=>{
+    res.send(
+        "<h1>Hello World</h1>"
+    )
+})
+
+const PORT=process.env.PORT || 8080;
+
+app.listen(PORT,()=>{
+    console.log(`Server running on ${process.env.DEV_MODE} mode on ${PORT}`.bgCyan.white)
+})
